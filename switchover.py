55661b35bba7735c4d6be45a68254a344b645524
Lucas Servén Marín <lserven@gmail.com>
2023-08-31 16:33:08 +0200

    format with black and fix typing
    Signed-off-by: Lucas Servén Marín <lserven@gmail.com>

--- switchover.py
+++ switchover.py
@@ -3,48 +3,62 @@ import json
 import argparse
 import subprocess
 
-def get_leader_pods(node_name: str, namespace: str = '') -> list[dict[str, str]]:
-    command = ["kubectl", "get", "pod",
-               "-l", "spilo-role=master", "-lapplication=spilo",
-               "--field-selector",
-               f"spec.nodeName={node_name}", "-ojson"]
+
+def get_leader_pods(node_name: str, namespace: str = "") -> list[dict[str, str]]:
+    command = [
+        "kubectl",
+        "get",
+        "pod",
+        "-l",
+        "spilo-role=master",
+        "-lapplication=spilo",
+        "--field-selector",
+        f"spec.nodeName={node_name}",
+        "-ojson",
+    ]
     if namespace:
-         command += ["-n", namespace]
+        command += ["-n", namespace]
     else:
         command += ["-A"]
     print(command)
 
-    process = subprocess.run(command,
-                             stdout=subprocess.PIPE,
-                             universal_newlines=True)
+    process = subprocess.run(command, stdout=subprocess.PIPE, universal_newlines=True)
 
     pods = json.loads(process.stdout)["items"]
-    return [{"namespace": x["metadata"]["namespace"], "name": x["metadata"]["name"]} for x in pods]
+    return [
+        {"namespace": x["metadata"]["namespace"], "name": x["metadata"]["name"]}
+        for x in pods
+    ]
 
 
-def switch(node_name: str, namespace: str = ''):
+def switch(node_name: str, namespace: str = "") -> None:
     pods = get_leader_pods(node_name, namespace)
     pre_command = ["kubectl", "exec", "-c", "postgres"]
     patronicmd = ["patronictl", "switchover", "--force"]
     for pod in pods:
         cluster_name = "-".join(pod["name"].split("-")[0:-1])
-        cmd = pre_command + ["-n", pod["namespace"], pod["name"], "--"] + patronicmd + [cluster_name, f"--leader={pod['name']}"]
+        cmd = (
+            pre_command
+            + ["-n", pod["namespace"], pod["name"], "--"]
+            + patronicmd
+            + [cluster_name, f"--leader={pod['name']}"]
+        )
         print(cmd)
         print(f"Executing switch over for {pod['name']} in {pod['namespace']}")
-        process = subprocess.run(cmd,
-                                 stdout=subprocess.PIPE,
-                                 universal_newlines=True)
+        process = subprocess.run(cmd, stdout=subprocess.PIPE, universal_newlines=True)
         print(process.stdout)
 
-def main():
+
+def main() -> None:
     parser = argparse.ArgumentParser(
-        prog='switchover',
-        description='Switchover all postgres leaders from one specific node',
+        prog="switchover",
+        description="Switchover all postgres leaders from one specific node",
     )
-    parser.add_argument('nodename', nargs=1)
-    parser.add_argument('-n', '--namespace', default="")
+    parser.add_argument("nodename", nargs=1)
+    parser.add_argument("-n", "--namespace", default="")
     args = parser.parse_args()
     switch(node_name=args.nodename[0], namespace=args.namespace)
+
 
 if __name__ == "__main__":
     main()
